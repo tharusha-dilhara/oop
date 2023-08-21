a8976640f584b1e8852ea6a8eaca0423619a31f6
tharusha dilhara jayadeera <104320538+tharusha-dilhara@users.noreply.github.com>
2023-08-21 19:40:33 +0530

    Create testoop.java

---
+++ extend/testoop.java
@@ -0,0 +1,17 @@
+package oop_tutorial;
+
+public class testoop {
+
+	public static void main(String[] args) {
+		
+		Senior s1=new Senior("tharusha",23,10);
+		s1.display();
+		
+		Senior s2=new Senior("gimani",3,30);
+		s2.display();
+		
+		
+
+	}
+
+}
