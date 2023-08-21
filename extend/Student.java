c6327ba157d3e25dc587e966e5112c04e8de7eef
tharusha dilhara jayadeera <104320538+tharusha-dilhara@users.noreply.github.com>
2023-08-21 19:39:51 +0530

    Create Student.java

---
+++ extend/Student.java
@@ -0,0 +1,24 @@
+package oop_tutorial;
+
+public class Student {
+
+	String name;
+	int age;
+	
+	public Student(String name,int age) {
+		this.name=name;
+		this.age=age;
+	}
+	
+	
+	public void setdate(String name,int age) {
+		this.name=name;
+		this.age=age;
+	}
+	
+	
+	
+	
+	
+
+}
